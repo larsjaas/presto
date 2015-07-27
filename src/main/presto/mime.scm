483483adf55b8a77569aab626581248284d80918
Lars J. Aas <lars.j.aas@gmail.com>
2015-07-27 23:32:19 +0200

    set utf-8 as charset for .css and .js

--- src/main/presto/mime.scm
+++ src/main/presto/mime.scm
@@ -3,10 +3,10 @@
   (cond ((equal? ".html" ext) '("text/html" "charset=utf-8"))
         ((equal? ".txt" ext) '("text/plain" "charset=utf-8"))
         ((equal? ".log" ext) '("text/plain" "charset=utf-8"))
-        ((equal? ".css" ext) "text/css")
+        ((equal? ".css" ext) '("text/css" "charset=utf-8"))
         ((equal? ".png" ext) "image/png")
         ((equal? ".jpg" ext) "image/jpeg")
         ((equal? ".pdf" ext) "application/pdf")
-        ((equal? ".js" ext) "application/javascript")
+        ((equal? ".js" ext) '("application/javascript" "charset=utf-8"))
         (else #f)))
 
