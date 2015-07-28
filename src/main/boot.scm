7534ec2e64436291072b8657ec320be555b6e6c2
Lars J. Aas <lars.j.aas@gmail.com>
2015-07-29 00:38:13 +0200

    support --config/-c <file> to name config file

--- src/main/boot.scm
+++ src/main/boot.scm
@@ -1,8 +1,9 @@
 #!/usr/bin/env TERM= chibi-scheme -r
 
 (import (chibi)
-        (chibi string)
-        (chibi config))
+        (chibi config)
+        (chibi filesystem)
+        (chibi string))
 (import (srfi 18)) ; make-thread
 (import (presto)
         (presto config)
@@ -20,46 +21,67 @@
                 (let* ((arg (string-split (car args) #\=))
                        (optionname (substring (car arg) 2))
                        (optionsym (string->symbol optionname))
-                       (value (join "=" (cdr arg))))
-                  (cond ((null? (conf-get *config* optionsym))
+                       (value (if (pair? (cdr arg))
+                                  (join "=" (cdr arg))
+                                  (car (cdr args)))))
+                  (cond ((eq? #f (conf-get *config* optionsym))
+                         (set! *config* (conf-set *config* optionsym value)))
+                        ((null? (conf-get *config* optionsym))
                          (set! *config* (conf-set *config* optionsym value)))
                         ((integer? (conf-get *config* optionsym))
                          (set! *config* (conf-set *config* optionsym (string->number value))))
                         (else
                          (set! *config* (conf-set *config* optionsym value))))
-                  (iter (cdr args))))))))
+                  (if (not (pair? (cdr arg)))
+                      (iter (cdr (cdr args)))
+                      (iter (cdr args)))))))))
 
 (define (responder-task)
   (presto-httpd (conf-get *config* 'http-port)))
 
 (define responder (make-thread responder-task))
 
+(define (get-config-file arguments)
+  (let iter ((args arguments))
+    (cond ((null? args)
+            #f)
+          ((or (string=? (car args) "--config")
+               (string=? (car args) "-c"))
+            (car (cdr args)))
+          (else
+            (iter (cdr args))))))
+
 (define (main arguments)
-  (set! *config* (conf-load "presto.conf"))
-  (update-config-settings (cddr arguments))
+  (let ((configfile (get-config-file arguments)))
+    (cond ((and configfile (not (file-exists? configfile)))
+            (display "no config '")
+            (display configfile)
+            (display "'.\n"))
+          (else
+            (set! *config* (conf-load (if configfile configfile "presto.conf")))
+            (update-config-settings (cddr arguments))
 
-  (set-config! *config*)
+            (set-config! *config*)
 
-  ; set up all pre-initialize settings, before initialize-calls fetches and
-  ; starts using the values
-  (if (not (null? (conf-get *config* 'access-log)))
-      (let ((logger (make-logger (conf-get *config* 'access-log)))
-            (stdout (make-logger (current-output-port))))
-        (logger 'append stdout)
-        (set-access-log-logger! logger)))
+            ; set up all pre-initialize settings, before initialize-calls fetches and
+            ; starts using the values
+            (if (not (null? (conf-get *config* 'access-log)))
+                (let ((logger (make-logger (conf-get *config* 'access-log)))
+                      (stdout (make-logger (current-output-port))))
+                  (logger 'append stdout)
+                  (set-access-log-logger! logger)))
 
 
-  (if (not (null? (conf-get *config* 'error-log)))
-      (let ((logger (make-logger (conf-get *config* 'error-log)))
-            (stderr (make-logger (current-error-port))))
-        (logger 'append stderr)
-        (set-error-log-logger! logger)))
+            (if (not (null? (conf-get *config* 'error-log)))
+                (let ((logger (make-logger (conf-get *config* 'error-log)))
+                      (stderr (make-logger (current-error-port))))
+                  (logger 'append stderr)
+                  (set-error-log-logger! logger)))
 
-  (presto-initialize)
+            (presto-initialize)
 
-  (if (conf-get *config* 'main-thread)
-      (responder-task)
-      (begin
-        (thread-start! responder)
-        (thread-join! responder)))
-  )
+            (if (conf-get *config* 'main-thread)
+                (responder-task)
+                (begin
+                  (thread-start! responder)
+                  (thread-join! responder)))))))
